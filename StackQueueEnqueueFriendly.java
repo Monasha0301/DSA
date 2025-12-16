d5b92bb1e904d48bb71a453178c38667eecbf9a6
Monasha0301 <monashabamunuarachchi@gmail.com>
2025-12-16 22:08:42 +0530

    Add files via upload

---
+++ StackQueueEnqueueFriendly.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class StackQueueEnqueueFriendly {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue operation (O(1))
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    // Dequeue operation (O(n))
+    int dequeue() {
+        if (s1.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        int dequeued = s2.pop();
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+
+        return dequeued;
+    }
+}
